e814bce400bee28b2f60433a431cc2f54ae54df8
ktvexe <ktvexe@gmail.com>
2016-09-27 07:52:46 +0800

    collect lastname to structure

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -7,21 +7,12 @@
  * line to set OPT properly */
 #define OPT 1
 typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
-    char *firstName_ptr;
-    char *email_ptr;
-    char *phone_ptr;
-    char *cell_ptr;
-    char *addr1_ptr;
-    char *addr2_ptr;
-    char *city_ptr;
-    char *state_ptr;
-    char *zip_ptr;
+    char lastName[128][MAX_LAST_NAME_SIZE];
+    unsigned char index;
     struct __PHONE_BOOK_ENTRY *pNext;
-
 } entry;
 
-entry *findName(char lastName[], entry *pHead);
+entry *findName(char lastname[], entry *pHead);
 entry *append(char lastName[], entry *e);
-
+void append_elements(char elements[16],char *info);
 #endif
